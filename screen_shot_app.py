e8eb9585296b6b0b6bef820bbc68396dc130afa5
Berlin Shaju Bellarmin <66897078+Berlinshaju@users.noreply.github.com>
2024-07-15 14:49:05 +0530

    Update screen_shot_app.py

--- screen_shot_app.py
+++ screen_shot_app.py
@@ -1,5 +1,3 @@
-# open cmd:- pip install virtualenv
-# open terminal:- pip install pyautogui
 import time
 import pyautogui
 
@@ -7,56 +5,33 @@ def screenshot():
     # Get current date and time in a formatted string
     current_time = time.strftime("%Y-%m-%d_%H-%M-%S")
     name = "C:/Users/Administrator/OneDrive/Documents/Screenshots/{}.png".format(current_time)
-    time.sleep(5)
+    time.sleep(5)  # Wait for 5 seconds (adjust as needed)
     img = pyautogui.screenshot(name)
     img.show()
 
+# Example usage:
 screenshot()
 
-#in different formats to save file:-
+# Different formats to save file:
 
-'''
-
-current path:
-
+# Current path and default date-time format (YYYY-MM-DD_HH-MM-SS):
+current_time = time.strftime("%Y-%m-%d_%H-%M-%S")
 name = "{}.png".format(current_time)
 
--------------------------------------------------
-
-import time
-
-current_time = time.strftime("%Y-%m-%d_%H-%M-%S")
-print(current_time)
-
---------------------------------------------------
-
-Date Only Format (YYYY-MM-DD):
-
+# Date Only Format (YYYY-MM-DD):
 current_date = time.strftime("%Y-%m-%d")
 name = "{}.png".format(current_date)
 
------------------------------------------------------
-Time Only Format (HH-MM-SS):
-
+# Time Only Format (HH-MM-SS):
 current_time = time.strftime("%H-%M-%S")
 name = "{}.png".format(current_time)
 
------------------------------------------------------
-Date and Time Combined (YYYY-MM-DD_HH-MM-SS):
-
-current_datetime = time.strftime("%Y-%m-%d_%H-%M-%S")
-name = "{}.png".format(current_datetime)
-
-
-Day of the Week (Monday, Tuesday, etc.):
-
+# Day of the Week (Monday, Tuesday, etc.):
 current_day = time.strftime("%A")
 name = "{}.png".format(current_day)
 
-
-Month and Day (MM-DD):
-
+# Month and Day (MM-DD):
 current_month_day = time.strftime("%m-%d")
 name = "{}.png".format(current_month_day)
 
-'''
+print(name)  # Example output filename
